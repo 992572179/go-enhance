601cfbe66c15bcb68675fb5418dac6be40f2f202
An avenue <992572179@qq.com>
2018-12-25 16:37:19 +0800

    slice的使用

--- class2/map.go
+++ class2/map.go
@@ -4,11 +4,25 @@ import "fmt"
 
 func main() {
 
-	stu := make(map[string]string)
-	stu["name"] = "z3"
-	stu["city"] = "Los Angeles"
-
-	//map[name:z3 class:class1]
+	stu := map[string]string{
+		"name":   "z3",
+		"city":   "Los Angeles",
+		"gender": "male",
+	}
 	fmt.Println(stu)
 
+	delete(stu, "name")
+	fmt.Println("after del:\t", stu)
+
+	for k, v := range stu {
+		fmt.Println(k, "\t", v)
+	}
+
+	dict := CreateMap()
+	dict["k1"] = "v1"
+	dict["k2"] = "v2"
+}
+
+func CreateMap() map[string]string {
+	return make(map[string]string)
 }
